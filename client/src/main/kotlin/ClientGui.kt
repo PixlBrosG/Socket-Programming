03a256c54fc9c099a2fa2385425e441409697af8
Pixl <pixelbrosg@gmail.com>
2025-09-23 10:39:01 +0200

    Implemented GUI

--- client/src/main/kotlin/ClientGui.kt
+++ client/src/main/kotlin/ClientGui.kt
@@ -3,92 +3,112 @@ package edu.ntnu.eliasei.smarttv
 import imgui.ImGui
 import imgui.app.Application
 import imgui.app.Configuration
+import imgui.type.ImInt
+import imgui.type.ImString
 import java.util.concurrent.CopyOnWriteArrayList
 
-class SmartRemoteApp(private val host: String, private val port: Int) : Application()
-{
-    private var connected = false
+class SmartRemoteApp(private val host: String, private val port: Int) : Application() {
+    private var connection = RemoteConnection()
     private val log = CopyOnWriteArrayList<String>()
 
-    override fun configure(config: Configuration)
-    {
+    private val hostInput = ImString(host, 64)
+    private val portInput = ImInt(port)
+    private val channelInput = ImInt(1)
+
+    override fun configure(config: Configuration) {
         config.title = "SmartTV Remote"
         config.width = 640
         config.height = 480
     }
 
-    override fun process()
-    {
+    override fun process() {
         ImGui.begin("SmartTV Remote")
 
-        ImGui.text("Host: $host")
+        val disabled = connection.isConnected
+        if (disabled) ImGui.beginDisabled()
+        ImGui.inputText("Host", hostInput)
         ImGui.sameLine()
-        ImGui.text("Port: $port")
+        ImGui.inputInt("Port", portInput)
+        if (disabled) ImGui.endDisabled()
 
-        if (!connected)
-        {
-            if (ImGui.button("Connect"))
-            {
-                // TODO: Implement connection logic
-                connected = true
-                log.add(">> Connected to $host:$port")
-            }
-        }
-        else
-        {
-            if (ImGui.button("Disconnect"))
-            {
-                // TODO: Implement disconnection logic
-                connected = false
+        if (connection.isConnected) {
+            if (ImGui.button("Disconnect")) {
+                connection.disconnect()
                 log.add(">> Disconnected from $host:$port")
+            }
+        } else {
+            if (ImGui.button("Connect")) {
+                connection.connect(hostInput.get(), portInput.get())
+                log.add(">> Connected to $host:$port")
             }
         }
 
         ImGui.separator()
 
-        if (connected)
-        {
-            if (ImGui.button("ON"))
-            {
+        if (connection.isConnected) {
+            if (ImGui.button("ON")) {
                 val cmd = CommandRegistry.get("ON")
-                // TODO: Send ON command using RemoteConnection
+                connection.send(cmd!!.build.invoke(emptyList()))
                 log.add(">> Sent: ON")
             }
             ImGui.sameLine()
-            if (ImGui.button("OFF"))
-            {
+            if (ImGui.button("OFF")) {
                 val cmd = CommandRegistry.get("OFF")
-                // TODO: Send OFF command using RemoteConnection
+                connection.send(cmd!!.build.invoke(emptyList()))
                 log.add(">> Sent: OFF")
             }
 
-            if (ImGui.button("Channel Up"))
-            {
+            if (ImGui.button("Channel Up")) {
                 val cmd = CommandRegistry.get("SET_CHANNEL")
-                // TODO: Send Channel Up command
-                log.add(">> Sent: SET_CHANNEL current+1")
+                connection.send(cmd!!.build.invoke(listOf("+1")))
+                log.add(">> Sent: SET_CHANNEL +1")
             }
             ImGui.sameLine()
-            if (ImGui.button("Channel Down"))
-            {
+            if (ImGui.button("Channel Down")) {
                 val cmd = CommandRegistry.get("SET_CHANNEL")
-                // TODO: Send Channel Down command
-                log.add(">> Sent: SET_CHANNEL current-1")
+                connection.send(cmd!!.build.invoke(listOf("-1")))
+                log.add(">> Sent: SET_CHANNEL -1")
             }
-        }
-        else
-        {
+
+            ImGui.inputInt("Channel", channelInput)
+            ImGui.sameLine()
+            if (ImGui.button("Set Channel")) {
+                val cmd = CommandRegistry.get("SET_CHANNEL")
+                connection.send(cmd!!.build.invoke(listOf(channelInput.get().toString())))
+                log.add(">> Sent: SET_CHANNEL ${channelInput.get()}")
+            }
+
+            ImGui.separator()
+
+            if (ImGui.button("Get Channel")) {
+                val cmd = CommandRegistry.get("GET_CHANNEL")
+                connection.send(cmd!!.build.invoke(emptyList()))
+                log.add(">> Sent: GET_CHANNEL")
+            }
+
+            if (ImGui.button("Get Channel Count")) {
+                val cmd = CommandRegistry.get("GET_CHANNELS")
+                connection.send(cmd!!.build.invoke(emptyList()))
+                log.add(">> Sent: GET_CHANNELS")
+            }
+        } else {
             ImGui.text("Connect to a TV to enable controls.")
         }
 
         ImGui.end()
 
+        // Drain all new messages from the connection
+        var msg = connection.pollMessage()
+        while (msg != null)
+        {
+            log.add(msg)
+            msg = connection.pollMessage()
+        }
+
         ImGui.begin("Log")
 
-        for (entry in log)
-        {
-            when
-            {
+        for (entry in log) {
+            when {
                 entry.startsWith("OK") || entry.startsWith("CHANNEL") ->
                     ImGui.textColored(0f, 1f, 0f, 1f, entry) // Green for success
                 entry.startsWith("ERROR") ->
@@ -104,7 +124,6 @@ class SmartRemoteApp(private val host: String, private val port: Int) : Applicat
     }
 }
 
-fun runGui(host: String, port: Int)
-{
+fun runGui(host: String, port: Int) {
     Application.launch(SmartRemoteApp(host, port))
 }
